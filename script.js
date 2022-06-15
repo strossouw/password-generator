81788a59be8f8c23d47dfb73c6d1589fc9a3d045
SRoss <srossouw@uwm.edu>
2022-06-15 05:29:17 -0500

    corrected code

--- script.js
+++ script.js
@@ -4,77 +4,55 @@ var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P"
 var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 var specialChar = ["!","@","#","$","%","^","&","*"];
 
+function generatePassword (){  
+var lengthPrompt = parseInt (window.prompt ("Enter password length? (Enter Number 8-128)"))
 
-var lengthPrompt = window.prompt ("Enter password length? (Enter Number 8-128)")
-
-var passwordLength = function(lengthPrompt) {
- /* if (lengthPrompt < 128 ) {
+  if (lengthPrompt < 8 ) {
     window.alert(lengthPrompt + " password is not long enough. Enter new length greater than 8")
-    // return to the beginning ??
+     return;
     }
-    else if (lengthPrompt > 8) {
+    if (lengthPrompt > 128) {
       window.alert(lengthPrompt + " password is too long. Enter new length less than 128 characters.")
+      return;
     }
-    else {
-      return passwordLength;
-    }
-}; */
- if (lengthPrompt > 7 && 129 < lengthPrompt) {
-     return passwordLength;
-    }
-    else { 
-      window.alert ("password does not fit length requirement")
-      passwordLength();
-    }
-   
-  };
-
-/*var upperConfirm = window.confirm("Include upper case letters?") 
-  
-  var upper = function (upperConfirm){
-    if (upperConfirm
-
-    }
-    else if (
-  } 
-  
- */
-
-/*var lowerPrompt = window.prompt("Include lower case letters? Yes or No?") 
-
-  var lower = function (lowerPrompt){
-    if (lowerPrompt === "yes" || upperPrompt === "YES"){
-     var lower = 
-    }
-     else (
+    if (Number.isNaN(lengthPrompt)){
+        window.alert(lengthPrompt + "input is not a number")
+        return;
     }
 
+    var upperConfirm = window.confirm("Include upper case letters?") 
+    var lowerConfirm = window.confirm("Include lower case letters?)")
+    var numbersConfirm = window.confirm("Include numbers?)")
+    var charConfirm = window.confirm("Include special characters?") 
 
-  } */
+    var passwordChar = []
+    var userChar = []
 
-/*var numbersPrompt = window.prompt("Include numbers? Yes or No?") 
-  var numbers = function (numbersPrompt){}
-    if (numberPrompt === "yes" || upperPrompt === "YES"){
-     var numbers = 
+    if (upperConfirm) {
+        userChar = userChar.concat(upperCase)
+        passwordChar.push(upperCase[Math.floor(Math.random()*upperCase.length)])
     }
-    else (
-
-
-    } */
-
-/*var charConfirm = window.prompt("Include special characters? Yes or No?") 
-  var numbers = function (charConfirm){
-  if (charConfirm
+    if (lowerConfirm) {
+        userChar = userChar.concat(lowerCase)
+        passwordChar.push(lowerCase[Math.floor(Math.random()*lowerCase.length)])
+    }   
+    if (numbersConfirm) {
+        userChar = userChar.concat(numbers)
+        passwordChar.push(numbers[Math.floor(Math.random()*numbers.length)])
+    }   
+    if (charConfirm) {
+        userChar = userChar.concat(specialChar)
+        passwordChar.push(specialChar[Math.floor(Math.random()*specialChar.length)])
     }
-    else (
 
+    var remaining = lengthPrompt-passwordChar.length
+        for (var i =0; i<remaining; i++){
+            passwordChar.push(userChar[Math.floor(Math.random()*userChar.length)])
+        }
 
-  } */
+        return passwordChar.join("");
+}
 
-  /* iterator 
-  (var i =0; i<passwordLength.length; i++){ and randomly puts in the values selected 
-    
-    */
 
 
 // Get references to the #generate element
